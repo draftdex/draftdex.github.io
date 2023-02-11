96d8e77615002c1c377b992d736d8f17961a3e81
John Moore <johnkmoore98@gmail.com>
2023-02-11 18:03:00 -0500

    Add all palette vars

--- src/app/authorization/login/login.component.ts
+++ src/app/authorization/login/login.component.ts
@@ -62,6 +62,22 @@ export class LoginComponent {
   }
 
   private setUserStyles(style: UserStyle): void {
+    document.documentElement.style.setProperty('--main-background', style.mainBackground);
     document.documentElement.style.setProperty('--header-background', style.headerBackground);
+    document.documentElement.style.setProperty('--header-text', style.headerText);
+    document.documentElement.style.setProperty('--main-text-dark', style.mainTextDark);
+    document.documentElement.style.setProperty('--main-text-light', style.mainTextLight);
+    document.documentElement.style.setProperty('--dark-background', style.darkBackground);
+    document.documentElement.style.setProperty('--light-background', style.lightBackground);
+    document.documentElement.style.setProperty('--button-color1', style.buttonColor1);
+    document.documentElement.style.setProperty('--button-color2', style.buttonColor2);
+    document.documentElement.style.setProperty('--button-text', style.buttonText);
+    document.documentElement.style.setProperty('--table-background1', style.tableBackground1);
+    document.documentElement.style.setProperty('--table-background2', style.tableBackground2);
+    document.documentElement.style.setProperty('--table-header', style.tableHeader);
+    document.documentElement.style.setProperty('--table-text', style.tableText);
+    document.documentElement.style.setProperty('--table-buttons', style.tableButtons);
+    document.documentElement.style.setProperty('--hyperlink_pc', style.hyperlinkPC);
+    document.documentElement.style.setProperty('--hyperlink-ac', style.hyperlinkAC);
   }
 }
